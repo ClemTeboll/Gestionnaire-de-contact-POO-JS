15ede9f5e67ac9fd6e0fc9436c0cc1e5da02d36a
Clément <clement.llobet@jasparo.fr>
2021-12-22 15:35:52 +0100

    Create class in new contact.js file

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+class Contact {
+    constructor(name, surname, email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    
+}
+
+export { Contact }
---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { Contact } from "./contact.js";
+
+let clement = new Contact("Llobet", "Clément", "clement.llobet@jasparo.fr");
+let erwan = new Contact("Morio", "Erwan", "erwan.morio@jasparo.fr");
+let fabrice = new Contact("Tripault", "Fabrice", "fabrice.tripault@jasparo.fr");
+let olivia = new Contact("Wojtala", "Olivia", "olivia.wojtala@jasparo.fr");
+
+console.log(clement);
