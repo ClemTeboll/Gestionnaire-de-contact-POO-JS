028899b738e38d03c344e1cfd9d07608f7909315
Clément <clement.llobet@jasparo.fr>
2021-12-22 16:39:55 +0100

    Create functions checkName and checkSurname, create regex rule to check email

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,15 @@
-class Contact {
+ class Contact {
     constructor(name, surname, email) {
         this.name = name;
         this.surname = surname;
         this.email = email;
     }
 
-    
+    displayInfo() {
+        name: this.name;
+        surname: this.surname;
+        email: this.email;
+    }
 }
 
 export { Contact }
--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ let erwan = new Contact("Morio", "Erwan", "erwan.morio@jasparo.fr");
 let fabrice = new Contact("Tripault", "Fabrice", "fabrice.tripault@jasparo.fr");
 let olivia = new Contact("Wojtala", "Olivia", "olivia.wojtala@jasparo.fr");
 
-console.log(clement);
+
+clement.displayInfo();
+erwan.displayInfo();
+fabrice.displayInfo();
+olivia.displayInfo();
+
+const checkName = (name) => {
+    if (name.length <= 2) {
+        prompt("Votre nom doit avoir plus de 2 caractères");
+    }
+}
+
+const checkSurname = (surname) => {
+    if (surname.length <= 2) {
+        prompt("Votre prénom doit avoir plus de 2 caractères");
+    }
+}
+
+const regex = /(?<name>.+)@(?<provider>\w+)\.(?<country>.+)/g;
